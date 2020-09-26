5fae08c3a57233eb21cc19dd225641ce6ba3f242
BarLvovski <barlvovski@gmail.com>
2020-09-26 13:58:11 -0700

    Add project files.

---
+++ trynode/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trynode
+{
+    public class Node<T>
+    {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+        public Node(T value, Node<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        public T GetValue()
+        {
+            return this.value;
+        }
+
+        public void SetValue(T value)
+        {
+            this.value = value;
+        }
+
+        public Node<T> GetNext()
+        {
+            return this.next;
+        }
+
+        public void SetNext(Node<T> next)
+        {
+            this.next = next;
+        }
+
+        public bool HasNext()
+        {
+            return (this.next != null);
+        }
+    }
+}
+
---
+++ trynode/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace trynode
+{
+
+    class grade
+    {
+        int course;
+        int gradevalue;
+
+        public grade(int mycourse , int mygradevalue)
+        {
+            course = mycourse;
+            gradevalue = mygradevalue;
+
+        }
+
+    }
+
+    class student
+    {
+        int shnatl;
+        int tz;
+        string name;
+        Node<grade> grades;
+
+        public student(int myshnat , int mytz , string myname)
+        {
+            
+            shnatl = myshnat;
+            tz = mytz;
+            name = myname;
+            //
+            grade g = new grade(10, 15);
+            grades  = new Node<grade>(g,null);
+            
+
+        }
+
+    }
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Node<int> pos;
+            
+
+
+        }
+
+    }
+}
