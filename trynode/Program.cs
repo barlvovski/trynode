4d30a3d90b8d7d9b2a92a6120574ee2475370f50
barlvovski <71371097+barlvovski@users.noreply.github.com>
2020-10-09 21:33:22 -0700

    0910_1

--- trynode/Program.cs
+++ trynode/Program.cs
@@ -13,9 +13,12 @@ namespace trynode
         {
             course = mycourse;
             gradevalue = mygradevalue;
-
         }
 
+        public string getCourseStr()
+        {
+            return (course.ToString() + " " + gradevalue.ToString()) ;
+        }
     }
 
     class student
@@ -28,38 +31,77 @@ namespace trynode
 
         public student(int myshnat , int mytz , string myname)
         {
-            
             shnatl = myshnat;
             tz = mytz;
             name = myname;
             //
             Random myRandom = new Random();
             int courses = myRandom.Next(4, 6);
+            Node<grade> currg = null;
+            Node<grade> nextg = null;
             for (int i = 0; i < courses; i++)
             {
-                grade g = new grade(10, 15);
-                grades = new Node<grade>(g, null);
-                
+          
+                grade g = new grade(myRandom.Next(10,20), myRandom.Next(60,100 ));
+                nextg = new Node<grade>(g, null);
+                            
+               
+               if (currg != null)    
+                currg.SetNext(nextg);
+                    
+               
+                currg = nextg;
+                if (i == 0)
+                {
+
+                    grades = currg;
+                }
+
+
             }
             
-            
+        }
+        public string getName()
+        {
+            return name;
+        }
 
+        public void printGrades()
+        {
+            Node<grade> gd = this.grades; 
+            while (gd != null)
+            {
+                                
+                grade gd1 = gd.GetValue();
+                Console.WriteLine(gd1.getCourseStr());
+                gd = gd.GetNext();
+            }
         }
 
     }
 
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            Node<int> pos;
+            const int MAXST = 19;
             
+            student [] st;
+            st = new student[MAXST];
+            for (int i = 0; i < MAXST; i++)
+            {
+                st[i] = new student(2004, 3090 + i, "name" + i.ToString());
+            }
 
-
+            for (int i = 0; i < MAXST; i++)
+            {
+                
+                Console.WriteLine("=================");
+                Console.WriteLine(st[i].getName());
+                st[i].printGrades();
+            }
         }
+
 
     }
 }
